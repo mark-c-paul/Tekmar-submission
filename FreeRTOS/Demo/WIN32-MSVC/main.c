d01c1b1e2586d16d598df7574be278a78cd36d8a
Mark C. Paul <markcpaul@sympatico.ca>
2022-06-06 19:23:59 -0700

    added semaphore to lock threads, created functions called by tasks and added comments

--- FreeRTOS/Demo/WIN32-MSVC/main.c
+++ FreeRTOS/Demo/WIN32-MSVC/main.c
@@ -6,6 +6,7 @@
  */
 
 #include "FreeRTOS.h"
+#include "semphr.h"
 #include "task.h"
 
 #define mainREGION_1_SIZE	8201
@@ -27,28 +28,42 @@ static BaseType_t xTraceRunning = pdTRUE;
 
 
 /*--------------------------------------------------------------------------------------------------*/
-/* My Code starts here                                                                              */
+/* My code starts here                                                                              */
 /*--------------------------------------------------------------------------------------------------*/
-
-TaskHandle_t myTask1Handle = NULL;
-TaskHandle_t myTask2Handle = NULL;
+SemaphoreHandle_t xSemaphore;
+TaskHandle_t populateArrayTaskHandle = NULL;
+TaskHandle_t searchArrayTaskHandle = NULL;
 int array[256];
 
-void myTask1(void* p) {
+//function populates the array with integers 0 through 255 representing ascii values 
+void populateArray() {
     int count;
     count = 0;
     while (count <= 255) {
         array[count] = count++;
     }
-    printf("A sorted array containing all ascii characters has been created\n");
-    return;
+    printf("A sorted array containing all ascii characters has been created.\n");
+    if (xSemaphoreGive(xSemaphore) == pdFALSE) { //tells other task it can start
+        configPRINTF("failed to give away semaphore\r\n");
+    }
 }
-void myTask2(void* p) {
+
+//function prompts the user for character inputs then uses a binary search to locate that input
+//in the array
+void searchArray() {
     char ch;
     int first, last, middle, search;
     while (1) {
+        if (xSemaphoreTake(xSemaphore, pdMS_TO_TICKS(1000))) { //waits for other task to finish before starting
+            break;
+        }
+        else {
+            configPRINTF("Could not take semaphore\n");
+        }
+    }
+    while (1) {
         printf("Which characters do you wish to locate using the binary search?\n");
-        scanf("%c", &ch);
+        ch = fgetc(stdin);
         search = ch;
         first = 0;
         last = 255;
@@ -60,27 +75,36 @@ void myTask2(void* p) {
                 printf("%c is located at index %d.\n", ch, middle);
                 break;
             }
-            else
+            else {
                 last = middle - 1;
+            }
             middle = (first + last) / 2;
         }
         if (first > last)
             printf("Not found! %c is not present in the list.\n", (char)search);
     }
-    return;
+}
+
+void populateArrayTask(void* p) {
+    populateArray();
+}
+void searchArrayTask(void* p) {
+    searchArray();
 }
 
 int main(void)
 {
     prvInitialiseHeap();
     vTraceEnable(TRC_START);
-
-    xTaskCreate(myTask1, "task1", 200, (void*)0, tskIDLE_PRIORITY, &myTask1Handle);
-    xTaskCreate(myTask2, "task2", 200, (void*)0, tskIDLE_PRIORITY, &myTask2Handle);
+    xSemaphore = xSemaphoreCreateBinary();
+    xTaskCreate(populateArrayTask, "populateArrayTask", 200, NULL, tskIDLE_PRIORITY, &populateArrayTaskHandle);
+    xTaskCreate(searchArrayTask, "searchArrayTask", 200, NULL, tskIDLE_PRIORITY, &searchArrayTaskHandle);
     vTaskStartScheduler();
     return 1;
 }
 
+/*--------------------------------------------------------------------------------------------------*/
+/* My code ends here                                                                              */
 /*--------------------------------------------------------------------------------------------------*/
 
 void vApplicationMallocFailedHook(void) {}
